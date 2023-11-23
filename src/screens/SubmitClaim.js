d8aca3424ea2d555dd5bdca0402f755fb455067b
kbasak <kausikbasak1999@gmail.com>
2023-11-23 18:11:23 +0530

    SubmitClaim -1

--- src/screens/SubmitClaim.js
+++ src/screens/SubmitClaim.js
@@ -24,7 +24,7 @@ const Claims = () => {
   const submit = () => {
     setpopupVisible(true);
   }
-  const onSubmitEditing=()=>{
+  const onSubmitEditing = () => {
     console.log("check")
     Keyboard.dismiss();
   }
@@ -54,42 +54,50 @@ const Claims = () => {
 
   return (
     <TouchableWithoutFeedback onPress={onSubmitEditing}>
-      <View style={{flex:1, padding:12}}>
+      <View style={{ flex: 1, padding: 12 }}>
 
-        <View style={styles.container}>
-          {renderLabel()}
-          <Dropdown
-            style={[styles.dropdown, (isFocus) ? { borderColor: 'orange' } : { borderColor: 'grey' }]}
-            placeholderStyle={styles.placeholderStyle}
-            selectedTextStyle={styles.selectedTextStyle}
-            inputSearchStyle={styles.inputSearchStyle}
-            iconStyle={styles.iconStyle}
-            data={data}
-            search
-            maxHeight={300}
-            labelField="label"
-            valueField="value"
-            placeholder={!isFocus ? 'Treatment Name' : '...'}
-            searchPlaceholder="Search..."
-            value={value}
-            onFocus={() => setIsFocus(true)}
-            onBlur={() => setIsFocus(false)}
-            onChange={item => {
-              setValue(item.value);
-              setIsFocus(false);
-            }}
-            renderLeftIcon={() => (
-              <AntDesign
-                style={styles.icon}
-                color={isFocus ? 'blue' : 'black'}
-                name="Safety"
-                size={20}
-              />
-            )}
-          />
+        <View style={{ margin: 20, maxHeight: 135, backgroundColor: 'white' }}>
+          <View style={styles.containerText}>
+            <Text style={styles.textFormat}>Choose Treatment Name: </Text>
+          </View>
+          <View style={styles.container}>
+            {renderLabel()}
+            <Dropdown
+              style={[styles.dropdown, (isFocus) ? { borderColor: 'orange' } : { borderColor: 'grey' }]}
+              placeholderStyle={styles.placeholderStyle}
+              selectedTextStyle={styles.selectedTextStyle}
+              inputSearchStyle={styles.inputSearchStyle}
+              iconStyle={styles.iconStyle}
+              data={data}
+              search
+              maxHeight={300}
+              labelField="label"
+              valueField="value"
+              placeholder={!isFocus ? 'Treatment Name' : '...'}
+              searchPlaceholder="Search..."
+              value={value}
+              onFocus={() => setIsFocus(true)}
+              onBlur={() => setIsFocus(false)}
+              onChange={item => {
+                setValue(item.value);
+                setIsFocus(false);
+              }}
+              renderLeftIcon={() => (
+                <AntDesign
+                  style={styles.icon}
+                  color={isFocus ? 'blue' : 'black'}
+                  name="Safety"
+                  size={20}
+                />
+              )}
+            />
+          </View>
         </View>
-        <View style={{ margin: 20, maxHeight: 80, backgroundColor: 'white' }}>
+        <View style={{ margin: 20, maxHeight: 125, backgroundColor: 'white' }}>
 
+          <View style={styles.containerText}>
+            <Text style={styles.textFormat}>Claim Details: </Text>
+          </View>
           <TextInput placeholder='Enter Claim Details' value={claim}
             onChangeText={(text) => { setClaim(text) }}
             onFocus={() => setIsClaimFocus(true)}
@@ -99,7 +107,10 @@ const Claims = () => {
         </View>
 
 
-        <View style={{ margin: 20, maxHeight: 80, backgroundColor: 'white' }}>
+        <View style={{ margin: 20, maxHeight: 125, backgroundColor: 'white' }}>
+          <View style={styles.containerText}>
+            <Text style={styles.textFormat}>Claim Amount: </Text>
+          </View>
 
           <TextInput placeholder='Enter Claim Amount $' value={amount}
             onChangeText={(text) => { setAmount(text) }}
@@ -122,7 +133,7 @@ const Claims = () => {
         <View>
           <Modal isVisible={popupVisible}>
             <View style={styles.modal}>
-              <Text style={{ fontSize: 26, paddingBottom:10, paddingTop:10, fontFamily:'sans-serif' }}><Text style={{color:'#00a800', fontWeight:'bold'}}>Congrats!</Text> You have Submitted Your Claim Successfully for <Text style={{color:'#ff8800ff', fontWeight:'bold'}}>${amount}</Text></Text>
+              <Text style={{ fontSize: 26, paddingBottom: 10, paddingTop: 10, fontFamily: 'sans-serif' }}><Text style={{ color: '#00a800', fontWeight: 'bold' }}>Congrats!</Text> You have Submitted Your Claim Successfully for <Text style={{ color: '#ff8800ff', fontWeight: 'bold' }}>${amount}</Text></Text>
               <PrimaryButton onPress={hideModal}>Okay</PrimaryButton>
             </View>
           </Modal>
@@ -151,22 +162,22 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     height: 200,
     width: '80%',
-    borderRadius:25,
+    borderRadius: 25,
     borderWidth: 2,
-    elevation:4,
+    elevation: 4,
     borderColor: '#313030',
     marginTop: 40,
     marginLeft: 40,
-    padding:8
+    padding: 8
   },
   container: {
     backgroundColor: 'white',
     padding: 0,
-    margin: 20,
-    maxWidth:'89%',
-    justifyContent:'center',
-    marginLeft:'5%',
-    marginTop:20
+    margin: 2,
+    maxWidth: '98%',
+    justifyContent: 'center',
+    marginLeft: '1%',
+    marginTop: 5
   },
   dropdown: {
     height: 55,
@@ -206,7 +217,20 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1
-  }
+  },
+  containerText: {
+    margin: 5,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flexDirection: "row",
+    width: "90%",
+  },
+  textFormat: {
+    fontSize: 22,
+    fontWeight: "700",
+    color: '#00006d',
+    fontFamily: 'notoserif'
+  },
 });
 
 export default Claims;
