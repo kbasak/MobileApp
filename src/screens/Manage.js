05c52ee709780a582a6bb71754ad81a87f95ac2f
hema2150058 <hemapriyavarkala@gmail.com>
2023-11-29 11:02:52 +0530

    Made changes to info and created manage component

---
+++ src/screens/Manage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {View,Text,StyleSheet} from 'react-native'
+
+const Manage = ()=>
+{
+return (
+
+    <View > 
+        <Text style={{color: "blue",marginBottom:"10%"}}>This is my manage screen</Text>
+        <Text>Product Offerings</Text>
+        <Text>Investment Opportunities</Text>
+        <Text>Performance Metrics</Text>
+        <Text>Customer Support</Text>
+        <Text>Inductry Insights</Text>
+    </View>
+);
+
+}
+
+const styles = StyleSheet.create({});
+
+export default Manage;
