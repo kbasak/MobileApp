cb5df93396af2d48618e2a10563187e4471e3aa7
kbasak <kausikbasak1999@gmail.com>
2024-01-11 14:03:34 +0530

    okta loading animation

--- src/screens/OktaLogin.js
+++ src/screens/OktaLogin.js
@@ -124,6 +124,30 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 16,
+  },
+  modalView: {
+    maxWidth: '60%',
+    maxHeight: '30%',
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 30,
+    paddingTop: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 100,
+  },
 });
 
 export default App1;
